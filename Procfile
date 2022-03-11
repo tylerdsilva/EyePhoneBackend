6b406d01fd5a14f12d0517497639037bd72e8234
tylerdsilva <tylerdsilva1@gmail.com>
2022-03-11 01:53:10 -0500

    Made procfile changes

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn app:app
+web: gunicorn eyephone:app --log-file=-
--- eyephone.py
+++ eyephone.py
@@ -8,9 +8,9 @@ app = Flask(__name__)
 CORS(app)
 
 
-# @app.route('/')
-# def hello():
-#     return 'Hello, World!'
+@app.route('/')
+def hello():
+    return 'Hello, World!'
 
 
 @app.route('/metrics/', methods=['POST'])
