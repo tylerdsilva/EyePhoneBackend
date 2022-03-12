79018409fc8f561b48cfd8d7c469a161d825e7d8
tylerdsilva <tylerdsilva1@gmail.com>
2022-03-12 02:25:36 -0500

    added cloud storage

--- eyephone.py
+++ eyephone.py
@@ -1,10 +1,26 @@
 import json
 import os
+import pyrebase
 
 from flask import Flask
 from flask import request
 from flask_cors import CORS
 
+firebaseConfig = {
+    "apiKey": "AIzaSyA7_Vtd1KtxTNsVphWKB6MmU1uOrR1LPU8",
+    "authDomain": "eyephone-51d80.firebaseapp.com",
+    "projectId": "eyephone-51d80",
+    "storageBucket": "eyephone-51d80.appspot.com",
+    "messagingSenderId": "745363148986",
+    "appId": "1:745363148986:web:286daba9bf395f0f3f7283",
+    "measurementId": "G-3GXCPPEFC8",
+    "databaseURL": "gs://eyephone-51d80.appspot.com/"
+}
+
+firebase = pyrebase.initialize_app(firebaseConfig)
+
+# define storage
+storage = firebase.storage()
 app = Flask(__name__)
 CORS(app)
 
@@ -18,15 +34,6 @@ def hello():
 def process_audio():
     FILE_OUTPUT = 'output.avi'
 
-    # Checks and deletes the output file
-    # You cant have a existing file or it will through an error
-    if os.path.isfile(FILE_OUTPUT):
-        os.remove(FILE_OUTPUT)
-
-    # opens the file 'output.avi' which is accessible as 'out_file'
-    with open(FILE_OUTPUT, "wb") as out_file:  # open for [w]riting as [b]inary
-        out_file.write(request.get_data())
-
     data = request.get_data()
     data_length = request.content_length
 
@@ -34,8 +41,17 @@ def process_audio():
         return 'File too large!', 400
 
     # process data here:
-    print("Processing data: ", type(data))
-    print(data)
+    print("Processing data: ", data)
+
+    # Checks and deletes the output file
+    # You cant have a existing file or it will through an error
+    if os.path.isfile(FILE_OUTPUT):
+        os.remove(FILE_OUTPUT)
+
+    # opens the file 'output.avi' which is accessible as 'out_file'
+    with open(FILE_OUTPUT, "wb") as out_file:  # open for [w]riting as [b]inary
+        out_file.write(data)
+
+    storage.child("output.avi").put("output.avi")
 
     return json.dumps({"diameter": 50, "constriction_velocity": 30}), 200
-
