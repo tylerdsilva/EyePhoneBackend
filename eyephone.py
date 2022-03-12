b4a00a4b54ce878468bf24e86539fa3bd9836498
tylerdsilva <tylerdsilva1@gmail.com>
2022-03-12 01:09:19 -0500

    Changed endpoint output

--- eyephone.py
+++ eyephone.py
@@ -1,4 +1,5 @@
 import json
+import os
 
 from flask import Flask
 from flask import request
@@ -15,6 +16,17 @@ def hello():
 
 @app.route('/metrics/', methods=['POST'])
 def process_audio():
+    FILE_OUTPUT = 'output.avi'
+
+    # Checks and deletes the output file
+    # You cant have a existing file or it will through an error
+    if os.path.isfile(FILE_OUTPUT):
+        os.remove(FILE_OUTPUT)
+
+    # opens the file 'output.avi' which is accessible as 'out_file'
+    with open(FILE_OUTPUT, "wb") as out_file:  # open for [w]riting as [b]inary
+        out_file.write(request.get_data())
+
     data = request.get_data()
     data_length = request.content_length
 
@@ -25,4 +37,5 @@ def process_audio():
     print("Processing data: ", type(data))
     print(data)
 
-    return json.dumps({"text": "Audio successfully processed!"}), 200
+    return json.dumps({"diameter": 50, "constriction_velocity": 30}), 200
+
